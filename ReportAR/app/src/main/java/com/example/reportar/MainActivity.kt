ee6b838a0860fb24e3d12b0ad03f0d5e0f879097
ValenElNoobato <valenelnoobatoyt@gmail.com>
2026-05-05 23:38:36 -0300

    Login sencillo junto con una pantalla simple de bienvenida

--- ReportAR/app/src/main/java/com/example/reportar/MainActivity.kt
+++ ReportAR/app/src/main/java/com/example/reportar/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.reportar
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etUser = findViewById<EditText>(R.id.etUser)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val user = etUser.text.toString()
+            val password = etPassword.text.toString()
+
+            if (user == "admin" && password == "123456") {
+                val intent = Intent(this, Welcome::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
