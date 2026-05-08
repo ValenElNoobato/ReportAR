5f0e20642a667dd55a36f2f32e1bb21315c3d35e
ValenElNoobato <valenelnoobatoyt@gmail.com>
2026-05-08 14:39:51 -0300

    Modificacion en el 'Acerca de' de la aplicacion

--- ReportAR/app/src/main/java/com/example/reportar/InfoFragment.kt
+++ ReportAR/app/src/main/java/com/example/reportar/InfoFragment.kt
@@ -1,7 +1,12 @@
 package com.example.reportar
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class InfoFragment : Fragment(R.layout.fragment_info) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
